4f5a3694273cd789914c4f1c4b1476b98c32820e
shaoke <shaokexu@gmail.com>
2016-05-25 09:33:48 +0800

    Add a shell script help to setup env in Raspberry PI 3

--- init-raspberry-pi-3.sh
+++ init-raspberry-pi-3.sh
@@ -13,4 +13,9 @@ curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
 # Install TightVNC
 sudo apt-get -y install tightvncserver
 
+# Setup git. Default set to my github account
+git config --global user.email "shaokexu@gmail.com"
+git config --global user.name "shaoke"
+
+
 
