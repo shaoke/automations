836bcb55192747480dee9c7430cd186f0dcc3e8d
shaoke <shaokexu@gmail.com>
2016-05-25 09:32:26 +0800

    Add a shell script help to setup env in Raspberry PI 3

---
+++ init-raspberry-pi-3.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# setup static ip address when connect use cable
+
+
+# install vim
+sudo apt-get -y update
+sudo apt-get -y install vim
+
+# install spf13-vim
+curl https://j.mp/spf13-vim3 -L > spf13-vim.sh && sh spf13-vim.sh
+
+# Install TightVNC
+sudo apt-get -y install tightvncserver
+
+
